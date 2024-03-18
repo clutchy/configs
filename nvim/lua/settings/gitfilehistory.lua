f9a6656838604a1b46a7f0608c7339c7ac6ba5db
Žiga Jerina <ziga.jerina@gmail.com>
2024-03-18 19:56:31 +0300

    Improve git file history browser

--- nvim/lua/settings/gitfilehistory.lua
+++ nvim/lua/settings/gitfilehistory.lua
@@ -1,57 +1,63 @@
-function GetFileHistoryCommits()
-  local handle = io.popen("git --no-pager log --follow --pretty=format:'%h %s' -- " .. vim.fn.expand("%"))
-  local stdout = handle:read("*a")
-  handle:close()
-  return stdout
-end
-
-function GetFileHistoryDiff(file, hash)
-  local handle = io.popen("git diff --no-prefix -U1000 " .. hash .. ".." .. hash .. "~1 -- " .. file)
-  local stdout = handle:read("*a")
-  handle:close()
-  return stdout
-end
-
-function OpenGitFileHistory(file, commitBuf, diffBuf, win)
-  vim.api.nvim_win_set_buf(win, diffBuf)
+function OpenGitFileHistoryDiff(file, commitBuf, diffBuf)
   local line = vim.fn.getline(".")
   local hash = string.gmatch(line, "%S+")()
-  local diff = GetFileHistoryDiff(file, hash)
-  local diffLine = 0
-  for s in diff:gmatch("[^\r\n]+") do
-    if diffLine < 5 then
-      diffLine = diffLine + 1
-    else
-      vim.fn.appendbufline(diffBuf, diffLine - 5, s)
-      diffLine = diffLine + 1
-    end
-  end
-
   vim.cmd('buffer ' .. diffBuf)
+  vim.cmd('%!' .. "git --no-pager diff --no-prefix -U1000 " .. hash .. ".." .. hash .. "~1 -- " .. file)
+  vim.cmd('normal dddddddddd')
   vim.cmd('setlocal filetype=diff')
   vim.cmd('buffer ' .. commitBuf)
 end
 
+function OpenGitHistoricFile(file, commitBuf, diffBuf, fileType)
+  local line = vim.fn.getline(".")
+  local hash = string.gmatch(line, "%S+")()
+  vim.cmd('buffer ' .. diffBuf)
+  vim.cmd('%!' .. "git --no-pager show " .. hash .. ":" .. file)
+  vim.cmd('setlocal filetype=' .. fileType)
+  vim.cmd('buffer ' .. commitBuf)
+end
+
+function OpenCommitDetails(commitBuf, diffBuf)
+  local line = vim.fn.getline(".")
+  local hash = string.gmatch(line, "%S+")()
+  vim.cmd('buffer ' .. diffBuf)
+  vim.cmd("%!git --no-pager log " .. hash .. "~1.." .. hash)
+  vim.cmd('normal Go')
+  vim.cmd('normal Gddpp0DOChanged files:')
+  vim.cmd("read !git --no-pager diff --name-only " .. hash .. ".." .. hash .. "~1")
+  vim.cmd('buffer ' .. commitBuf)
+end
+
 function GitFileHistory()
-  local commits = GetFileHistoryCommits()
   local file = vim.fn.expand("%")
   local oldWin = vim.api.nvim_get_current_win()
   local oldBuf = vim.api.nvim_win_get_buf(oldWin)
+  local fileType = vim.bo.filetype
   vim.cmd('vsplit')
   vim.cmd('vertical resize 70')
   local win = vim.api.nvim_get_current_win()
   local commitBuf = vim.api.nvim_create_buf(false, false)
   local diffBuf = vim.api.nvim_create_buf(false, false)
   vim.api.nvim_win_set_buf(win, commitBuf)
-  local fileHistoryLines = 0
-  for s in commits:gmatch("[^\r\n]+") do
-    fileHistoryLines = fileHistoryLines + 1
-    vim.fn.append(vim.fn.line('$') - 1, s)
-  end
+  vim.api.nvim_win_set_buf(oldWin, diffBuf)
+
+  vim.cmd('buffer ' .. commitBuf)
+  vim.cmd('%!' .. "git --no-pager log --pretty=format:'\\%h \\%al (\\%ai) \\%s' -- " .. file)
+  local lines = vim.fn.getline(1, "$")
+  local hasCommits = lines[1] ~= '' and lines[1] ~= nil
 
   vim.keymap.set('n', '<CR>', function()
-      vim.api.nvim_buf_set_lines(diffBuf, 0, -1, true, {})
-      OpenGitFileHistory(file, commitBuf, diffBuf, oldWin)
+      OpenGitFileHistoryDiff(file, commitBuf, diffBuf)
+    end,
+    { silent = true, buffer = commitBuf })
+
+  vim.keymap.set('n', 'o', function()
+      OpenGitHistoricFile(file, commitBuf, diffBuf, fileType)
+    end,
+    { silent = true, buffer = commitBuf })
+
+  vim.keymap.set('n', 'd', function()
+      OpenCommitDetails(commitBuf, diffBuf)
     end,
     { silent = true, buffer = commitBuf })
 
@@ -62,13 +68,11 @@ function GitFileHistory()
     vim.cmd('bw! ' .. diffBuf)
   end, {})
 
-  vim.fn.setcursorcharpos(1, 1)
-  vim.fn.deletebufline(commitBuf, vim.fn.line('$'))
   vim.cmd('setlocal nowrap')
   vim.cmd('setlocal cursorline')
 
-  if fileHistoryLines > 0 then
-    OpenGitFileHistory(file, commitBuf, diffBuf, oldWin)
+  if hasCommits then
+    OpenGitFileHistoryDiff(file, commitBuf, diffBuf)
   end
 end
 
